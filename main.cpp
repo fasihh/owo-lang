245850834392362fb36c31669152a2d3089a91e5
fasihh <fhk.tcs@gmail.com>
2025-03-23 23:19:39 +0500

    added main

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <owo>
+
+int main(int argc, char *argv[]) {
+  try {
+    if (argc > 2) {
+      std::cout << "Usage: owo [script]" << std::endl;
+      exit(64);
+    } else if (argc == 2) {
+      owo::run_file(argv[1]);
+    } else {
+      owo::run_prompt();
+    }
+  } catch (const std::runtime_error& error) {
+    std::cout << error.what() << std::endl;
+  }
+
+  Token t(TokenType::STRING, "example", std::string("Hello World!"), 0);
+
+  std::cout << t << std::endl;
+
+  return 0;
+}
